ae64320f0f4d840233caf646baafdff47615108a
André Kapelrud <andre.kapelrud@sintef.no>
2025-07-02 13:24:14 +0200

    Working batch script. 1 level of array jobs

--- Exec/Examples/StreamerIntegralCriterionBatch/parameter_space.py
+++ Exec/Examples/StreamerIntegralCriterionBatch/parameter_space.py
@@ -14,43 +14,11 @@ pspace = {
             "uri" : ["gas", "law", "my_ideal_gas", "pressure"],
             "values" : list(np.arange(1e5, 11e5, 1e5))
             },
-        "photoionization" : {
-            "target" : "chemistry",
-            "uri" : [
-                "photoionization",
-                [
-                    '+["reaction"=<chem_react>"Y + (O2) -> e + O2+"]', # non-optional match
-                    '*["reaction"=<chem_react>"Y + (O2) -> (null)"]' # optional match (create-if-not-exists)
-                    ],
-                "efficiency"
-                ],
-            "values" : [[float(v), float(1.0-v)] for v in np.arange(0.0, 1.0, 0.25)]
+        "geometry_radius" : {
+            "target" : "input",
+            "uri" : "Vessel.rod_radius",
+            "values" : [ [0.0, 25e-3] ]
             },
-        "initial particles" : {
-            "target" : "chemistry",
-            "disparate" : False,
-            "uri" : [
-                "plasma species",
-                '+["id"="e"]', "initial particles",
-                '+["tag"="change-me"]', "gaussian distribution",
-                [ "center", "radius" ]
-                ],
-            "values" : [
-                [ [0, 0.04, 0], 0.04 ]  # one tuple: [ center, radius ]
-                ]
-            },
-        #"rod_gap" : {
-        #    "target" : "input",
-        #    "uri" : ["Vessel.rod_point"],
-        #    "values" : [ [0.0, 25e-3] ]
-        #    },
-        #"rod_radius" : {
-        #    "target" : "input",
-        #    "uri" : ["Vessel.rod_radius"],
-        #    "values" : [
-        #        10E-3
-        #        ]
-        #    }
         }
 
 if __name__ == '__main__':
